88ad386c10322ecc22020565fa5d2ec6c79a661d
Anderson <anderson@ingenico-01-111.dsc.ufcg.edu.br>
2017-12-07 11:54:24 -0300

    renomeando arquivos

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -3,7 +3,7 @@ import { navModel } from './nav-menu-model';
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: ['./nav-menu.component.scss']
 })
 export class NavMenuComponent implements OnInit {
 
